dc81b34f1071df5e778996d0864341098a097b64
zhemdbs <zhemdbs@gmail.com>
2021-07-30 15:19:10 +0900

    이모지 추가

--- js/weather.js
+++ js/weather.js
@@ -14,7 +14,7 @@ function onGeoOK(position) {
     });
 }
 function onGeoError() {
-  alert("Can't find you. No weather for you.")
+  alert("Can't find you. No weather for you.😓")
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
