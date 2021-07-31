0da87fdec8be781f568c3414f7ae927a6533f038
zhemdbs <zhemdbs@gmail.com>
2021-07-31 23:36:03 +0900

    temp 추가

--- js/weather.js
+++ js/weather.js
@@ -3,14 +3,14 @@ const API_KEY = "3ffad7c481e641d6705636c755fec75c";
 function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   fetch(url)
     .then(response => response.json())
     .then(data => {
       const city = document.querySelector(".city");
       const weather = document.querySelector(".weather");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 function onGeoError() {
